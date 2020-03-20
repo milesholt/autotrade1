6c38106aa6e82c23af1878f2965b4e8b7ac04f26
Miles Holt <milesholt@s.obscuraproductions.com>
2020-03-20 11:34:17 +0000

    Change margin2 to 200 instead of 500 for more accuracy

--- strategies/breakoutStrategy.js
+++ strategies/breakoutStrategy.js
@@ -71,7 +71,7 @@ actions.calcResistSupport2 = async function(pricedata,type){
 
   let prices = pricedata[type].map(r => (parseInt((r.high+r.low)/2).toFixed(2)));
   let margin1 = 50;  // Small margin, concentrating on the largest cluster of prices that fit within it, becoming the midrange line
-  let margin2 = 500; // High margin, to search for prices stemming from the midrange line, becoming support and resistance lines
+  let margin2 = 200; // High margin, to search for prices stemming from the midrange line, becoming support and resistance lines
   let matches = []; // Matches for midrange line
   let matches2 = []; // Matches for support and resistance lines
   let line = 0;
