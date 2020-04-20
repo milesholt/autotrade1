743af11d06b10701104d29f6eb46f9927dcc9a62
milesholt <contact@milesholt.co.uk>
2020-04-21 00:05:24 +0100

    Update main.js
    Changed rangeConfirmationLimit to 15

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ const monitor = require('./services/monitor.js');
 
 //Parameters
 const rangelimit = 100;
-const rangeConfirmationLimit = 10;
+const rangeConfirmationLimit = 15;
 let check0 = false, check0_2 = false, check1 = false, check2 = false, check3 = false, check4 = false, check5 = false, check6 = false, check7 = false;
 let prices = [];
 let pricedata = {'support': [], 'resistance': []};
