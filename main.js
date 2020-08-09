4f2aaec4ff945d26d7f8f4b151fa48fa679d5336
milesholt <contact@milesholt.co.uk>
2020-08-09 10:28:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -481,7 +481,7 @@ if(noError){
       let ntime = moment(price.time);
       let diff = Math.abs(time.diff(ntime, 'minutes'));
       console.log('old time: ' + time + ' new time: ' + ntime + ' diff: ' + diff);
-      if(diff !== 60) totalMissingHours = diff / 60;
+      if(diff !== 60) totalMissingHours += diff / 60;
       time = moment(price.time);
     }
   });
