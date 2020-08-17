22e8f909b2ea7034c946e697e75eab9206334c74
milesholt <contact@milesholt.co.uk>
2020-08-17 11:42:37 +0100

    Update stream.js

--- services/stream.js
+++ services/stream.js
@@ -15,7 +15,7 @@ var subscriptionMode = 'MERGE';
 var items = ['CHART:'+epic+':HOUR'];
 var fields = ['UTM','LTV', 'OFR_OPEN','OFR_CLOSE','OFR_HIGH','OFR_LOW','BID_OPEN','BID_CLOSE','BID_HIGH','BID_LOW'];
 
-//todo: setup monitoring when deal is mate to monitor status as it's being processed
+//todo: setup monitoring when deal is made to monitor status as it's being processed
 //sometimes a deal will be created but the confirm will be dealnotfound and it wont be processed, we need to subscribe to stream to see whats happening
 //var streamLogDir = path.join(__dirname, 'stream_DealCreateMonitor.json');
 //var items = ['TRADE:'+process.env.IG_IDENTIFIER];
