afcd3a1a38b43e8b4a891a5f97af7982089e3ea3
milesholt <contact@milesholt.co.uk>
2020-10-11 14:25:28 +0100

    Update monitor.js
    Added check to ensure price data is not returning NaN. Will stop monitoring if this happens

--- services/monitor.js
+++ services/monitor.js
@@ -126,6 +126,14 @@ actions.beginMonitor = async function(){
                       'lastTraded': null
                     },
                     'lastTradedVolume': parseFloat(data[3])
+                  }'
+                  
+                  //if monitoring a position over the weekend when market is closed, the monitoring will freeze. So we need to stop monitoring when a value is undefined (market has closed).
+                  //in this case, either d.closePrice.ask or d.closePrice.bid will be 'NaN' when this happens
+                  
+                  if (isNaN(d.closePrice.ask) || isNaN(d.closePrice.bid)) {
+                    console.log('price data is returning NaN, market has potentially closed while monitoring. Stopping monitoring...');
+                    actions.stopMonitor();
                   }
 
                   //if stream price goes beyond settings, take action
