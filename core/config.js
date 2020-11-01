ca973ecdf123938d146f30b3c3382d4f8c283a76
Miles Holt <contact@milesholt.co.uk>
2020-11-01 16:49:18 +0000

    Checking market data

--- core/config.js
+++ core/config.js
@@ -14,7 +14,8 @@ missingHoursLimit = 3;
 bumpgrouplimit = 5;
 
 //Main parameters
-markets = [{'alias':'stellar', 'epic' : 'CS.D.XLMUSD.TODAY.IP', 'dealId': '', 'data': {}, 'trades': [], 'errors':[]}];
+//markets = [{'alias':'stellar', 'epic' : 'CS.D.XLMUSD.TODAY.IP', 'data': {}, 'trades': [], 'errors':[]}];
+markets = [];
 market = {};
 trade = { 'dealId': '', 'startAnalysis': '', 'closeAnalysis': '', 'start_timestamp': '', 'start_date':'', 'end_timestamp': '', 'end_date': '' };
 mid = 0;
