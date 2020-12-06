4fd5a3971c9d7a5e2c5b5310ff3a6fdb8bf60d34
milesholt <contact@milesholt.co.uk>
2020-12-06 17:31:45 +0000

    Update config.js

--- core/config.js
+++ core/config.js
@@ -5,6 +5,7 @@ check0 = false, check0_2 = false, check1 = false, check2 = false, check3 = false
 momentLimitPerc = 0.1;
 rangeLimitPerc = 0.1;
 tradeLimitPerc = 0.3;
+rangeAreaLimitPerc = 0.3;
 lineDistanceLimitPerc = 0.05;
 
 momentumLimit = 0;
