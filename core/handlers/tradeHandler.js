05026df5d5f4557faade735745bb57926857d51c
Miles Holt <contact@milesholt.co.uk>
2021-03-04 23:31:42 +0000

    Updated

--- core/handlers/tradeHandler.js
+++ core/handlers/tradeHandler.js
@@ -37,8 +37,8 @@ actions.determineTrade = async function(){
 
   //If all checks pass, begin trade
   //TODO: Move checks to specific strategy
-  //const checks = [check0,check1,check2,check5,check6,check7,check8,check9,check10,check11,check12];
-  const checks = [true];
+  const checks = [check0,check1,check2,check5,check6,check7,check8,check9,check10,check11,check12];
+  //const checks = [true];
   if(checks.indexOf(false) == -1){
 
       console.log('All checks passed. Beginning trade...');
