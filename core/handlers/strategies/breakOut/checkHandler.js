185eb29d2c5eea2b0af256faa1b95eefed621781
Miles Holt <contact@milesholt.co.uk>
2021-03-08 11:50:19 +0000

    Updated

--- core/handlers/strategies/breakOut/checkHandler.js
+++ core/handlers/strategies/breakOut/checkHandler.js
@@ -111,8 +111,8 @@ actions.checkOpenTrade = async function(){
   //console.log(market.deal);
   if(lib.isEmpty(market.deal)) {
     console.log('Deal on market data is empty. Checking for open positions.');
-    console.log('market epic: ' + market.epic);
-    console.log(market.deal);
+    console.log('market epic: ' + markets[mid].epic);
+    console.log(markets[mid].deal);
     await actions.checkDeal();
   } else {
     //deal is in process for this market, get trade data
