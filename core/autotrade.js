5641b208a6394a4d8282bd7e623f2f9aea93148c
Miles Holt <contact@milesholt.co.uk>
2021-03-10 14:06:02 +0000

    Update

--- core/autotrade.js
+++ core/autotrade.js
@@ -295,6 +295,7 @@ actions.exec = async function(){
     await tradeHandler.actions.determineTrade();
 
     //Final logs
+    console.log(market);
     markets[mid] = market;
     await cloudHandler.actions.updateFile(markets,marketDataDir);
 
