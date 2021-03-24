9e267ad4f8a4f2f9599cc31a801914d2c68a73e7
Miles Holt <contact@milesholt.co.uk>
2021-03-24 11:25:03 +0000

    Updated

--- core/services/analytics.js
+++ core/services/analytics.js
@@ -431,7 +431,7 @@ actions.drawChart = async function(priceData, lineData, analysis, rangeData){
       wavecount: rangeData.wavecount
     }
 
-    let json = 'var '+alias+'='+JSON.stringify(d);
+    let json = 'var '+market.epic+'='+JSON.stringify(d);
 
     var jsonDir = 'core/data/plotly/'+market.epic+'.js';
     cloud.updateFile(json,jsonDir);
