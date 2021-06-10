cc47060df61cc944b9bd3a202fd03bcf27a6fd39
Miles Holt <contact@milesholt.co.uk>
2021-06-10 10:21:45 +0100

    Updated

--- core/handlers/strategies/breakOut/analysisHandler.js
+++ core/handlers/strategies/breakOut/analysisHandler.js
@@ -229,7 +229,7 @@ actions.finalAnalysis = async function(){
     'stopDistanceLevel': stopDistanceLevel,
     'limitDistance': limitDistance,
     'limitDistanceLevel': limitDistanceLevel,
-    'ticket': lib.isEmpty(market.deal) == false ? market.deal.ticket : {}
+    'ticket': lib.isEmpty(market.deal) == false ? market.deal : {}
   };
 
 
