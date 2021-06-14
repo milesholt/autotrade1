bc1cc47d4b5c5ae81e20fb70003152b2275df831
Miles Holt <contact@milesholt.co.uk>
2021-06-14 17:44:47 +0100

    Updated

--- core/autotrade.js
+++ core/autotrade.js
@@ -90,6 +90,23 @@ This resets any default variables for each loop
 actions.setDefaults = async function(){
   //Main variables
   check0 = false, check0_2 = false, check1 = false, check2 = false, check3 = false, check4 = false, check5 = false, check6 = false, check7 = false, check8 = false, check9 = true, check10 = true, check11 = true, check12 = true; check13 = false; check14 = false; check15 = false;
+
+  checks = {
+    '___4HoursNotRanging': {'is':false}, //check14
+    '___noBumpInRange': {'is':true}, //check11
+    '___noVolatileGap': {'is':false}, //check10
+    '___breakingThroughRange': {'is':false}, //check8
+    '___withinTradeThreshold': {'is':true}, //check9
+    '___beforeRangeTrendNotBroken': {'is':false}, //check13
+    '___recentTrendSameAsTrend': {'is':false}, //check6
+    '___beforeRangeSameAsTrend': {'is':false}, //check7
+    '___rangeConfirmationsGreaterThanLimit': {'is':false}, //check2
+    '___rangeAreaGood': {'is':false}, //check0
+    '___enoughWaves': {'is':false}, //check15
+    '___notTradedBefore': {'is':false}, //check12
+    '___enoughConfirmations': {'is':false}
+  }
+
   rangeData = {'resistance': {}, 'support': {}, 'bumps': [], 'waves': [], 'wavecount': 0};
   lineData = {'support': 0, 'resistance': 0, 'midrange': 0};
   confirmationData = { 'waves':[], 'confirmationPoints': [], 'trendPoints' : [] };
