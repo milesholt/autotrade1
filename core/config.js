9b14750ab49f4fe46f952a21e2f251ebb43ea9cf
Miles Holt <contact@milesholt.co.uk>
2021-06-22 11:54:12 +0100

    Updated

--- core/config.js
+++ core/config.js
@@ -46,7 +46,7 @@ rangeLimitPerc = 0.45;
 tradeLimitPerc = 0.3;
 lineDistanceLimitPerc = 0.05;
 limitDistancePerc = 0.4;
-limitClosePerc = 0.5;
+limitClosePerc = 0.65;
 stopClosePerc = 1;
 
 momentumLimit = 0;
