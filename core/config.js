0ecdcd814d3797956e8253ccac5aaf1c02e23609
Miles Holt <contact@milesholt.co.uk>
2022-01-04 16:30:37 +0000

    Updated

--- core/config.js
+++ core/config.js
@@ -133,7 +133,7 @@ markets = [
    // {'id':1, 'alias': 'NEO', 'epic': 'CS.D.NEOUSD.TODAY.IP', 'data': {}, 'deal': {}, 'tradedbefore' : false }
    //{'id':1, 'alias': 'Ripple', 'epic': 'CS.D.XRPUSD.TODAY.IP', 'data': {}, 'deal': {}, 'tradedbefore' : false, 'stopDistancePerc' : '0.85', 'minimumStop' : {'value': 10, 'type': 'points'}  }
     // {'id':0, 'alias': 'Volatility Index', 'epic': 'CC.D.VIX.USS.IP', 'data': {}, 'deal': {}, 'tradedBefore' : false, 'stopDistancePerc' : 0.4, 'limitDistancePerc' : 0.2, 'minimumStop' : {'value': null, 'type': 'points'}, 'size': 1  },
-   {'id':0, 'alias': 'Carbon Emissions', 'epic': 'CO.D.CFI.Month1.IP', 'data': {}, 'deal': {}, 'tradedBefore' : false, 'stopDistancePerc' : 0.4, 'limitDistancePerc' : 0.4, 'minimumStop' : {'value': null, 'type': 'points'}, 'size': 1, 'marketClosed' : [16,8]  },
+   {'id':0, 'alias': 'Carbon Emissions', 'epic': 'CO.D.CFI.Month1.IP', 'data': {}, 'deal': {}, 'tradedBefore' : false, 'stopDistancePerc' : 0.4, 'limitDistancePerc' : 0.4, 'minimumStop' : {'value': null, 'type': 'points'}, 'size': 1, 'marketClosed' : [16,7]  },
 
    {'id':1, 'alias': 'MXNJPY', 'epic': 'CS.D.MXNJPY.TODAY.IP', 'data': {}, 'deal': {}, 'tradedBefore' : false, 'stopDistancePerc' : 0.4, 'limitDistancePerc' : 0.4,'minimumStop' : {'value': null, 'type': 'points'}, 'size': 5, 'marketClosed' : [0,0]  },
    //{"id":2, "alias": "Spot Platinum", "epic": "CS.D.PLAT.TODAY.IP", "data": {}, "deal": {}, "tradedbefore" : false, "stopDistancePerc" : "0.85", "minimumStop" : {"value": 10, "type": "points"}  }
