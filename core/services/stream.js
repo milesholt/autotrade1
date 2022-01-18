56e7b93d1fe5d8c56ad886bbed6634ad7566c067
Miles Holt <contact@milesholt.co.uk>
2022-01-18 17:12:15 +0000

    Updated

--- core/services/stream.js
+++ core/services/stream.js
@@ -155,7 +155,10 @@ actions.checkSubscriptions = async function(){
             }
         });
 
-        if(add) ch_epics.push(id);
+        if(add) {
+          console.log('adding epic');
+          ch_epics.push(id);
+        }
       });
     } catch(e) {
       console.log(e);
