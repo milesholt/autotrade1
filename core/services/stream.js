e85492af33857a50f62a6725257aab4897f5155b
Miles Holt <contact@milesholt.co.uk>
2022-02-04 14:00:14 +0000

    Updated

--- core/services/stream.js
+++ core/services/stream.js
@@ -162,6 +162,9 @@ actions.checkSubscriptions = async function(){
             //Return active subscriptions
             ch_subscriptions =  await api.getActiveSubscriptions();
 
+            console.log('subscriptions:');
+            console.log(ch_subscriptions);
+
             //Loop through all active subscriptions and check EPIC ID, if there is more than one ID, then remove.
             //Each subscription we log the IP into epics array, and if it occurs again, we remove that one
             ch_subscriptions.forEach(subscription => {
