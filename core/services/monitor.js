af0dd911dc432bdcbfcce6420c65e00689a0b7ee
milesholt <contact@milesholt.co.uk>
2022-02-08 21:23:14 +0000

    Update monitor.js

--- core/services/monitor.js
+++ core/services/monitor.js
@@ -431,8 +431,20 @@ actions.beginMonitor = async function(dealId,dealRef,epic,streamLogDir,attempt =
                                 if(dir == 'SELL' && d.closePrice.ask <= x.newLimit) markets[x.marketId].closeprofit = true;
 
                                 //use new stop level
-                                if(dir == 'BUY' && d.closePrice.bid <= x.newStop) markets[x.marketId].closeloss = true;
-                                if(dir == 'SELL' && d.closePrice.ask >= x.newStop) markets[x.marketId].closeloss = true;
+                                if(dir == 'BUY' && d.closePrice.bid <= x.newStop) {
+                                    console.log('direction is buy.');
+                                    console.log('bid price less than new stop');
+                                    console.log('d.closePrice.bid: ' + d.closePrice.bid):
+                                    console.log('x.newStop: ' + x.newStop);
+                                    markets[x.marketId].closeloss = true;
+                                }
+                                if(dir == 'SELL' && d.closePrice.ask >= x.newStop){
+                                    console.log('direction is sell.');
+                                    console.log('ask price greater than new stop');
+                                    console.log('d.closePrice.ask: ' + d.closePrice.ask):
+                                    console.log('x.newStop: ' + x.newStop);
+                                    markets[x.marketId].closeloss = true;
+                                }
 
                                 let closePrice = dir == 'BUY' ? d.closePrice.bid : d.closePrice.ask;
                                 let foundMonitor =  false;
