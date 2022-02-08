44d359928b943d2f10587b5fddc6125edc09fa35
Miles Holt <contact@milesholt.co.uk>
2022-02-08 00:30:45 +0000

    Updated

--- core/handlers/trendHandler.js
+++ core/handlers/trendHandler.js
@@ -49,17 +49,27 @@ actions.determineMid4HourTrend = async function(){
   midtrend4HoursDiff = parseFloat(Math.abs(mid4HoursClose - last4HoursClose).toFixed(2));
   midtrend4HoursDiffPerc = parseFloat(Math.abs((midtrend4HoursDiff / priceDiff4Hours) * 100).toFixed(2));
 
+  midtrend4Hours = 'ranging';
+
+  console.log('midtrend4HoursDiffPerc: ' + midtrend4HoursDiffPerc);
+  console.log('midtrend4HourLimitPerc: ' + midtrend4HourLimitPerc);
+
   //trend4HoursDiffPerc = Math.abs(100 - (first4HoursClose / last4HoursClose * 100)).toFixed(2);
 
   //if prices have moved and over significant distance (range area limit) - 40%
-  if((mid4HoursClose > last4HoursClose) && (midtrend4HoursDiffPerc  >= midtrend4HourLimitPerc)) {
-    console.log('mid4hours is bearish');
-    midtrend4Hours = 'bearish';
+  if(midtrend4HoursDiffPerc >= midtrend4HourLimitPerc){
+    console.log('midtrend4HoursDiffPerc is above or equal to limit: ' + midtrend4HourLimitPerc);
+    if(mid4HoursClose > last4HoursClose) {
+      console.log('mid4hours is bearish');
+      midtrend4Hours = 'bearish';
+    } else if(last4HoursClose > mid4HoursClose){
+      console.log('mid4hours is bullish');
+      midtrend4Hours = 'bullish';
+    } else {
+      midtrend4Hours = 'ranging';
+    }
   }
-  if((last4HoursClose > mid4HoursClose) && (midtrend4HoursDiffPerc >= midtrend4HourLimitPerc)){
-    console.log('mid4hours is bullish');
-    midtrend4Hours = 'bullish';
-  }
+
   console.log('mid4hourstrend: ' + midtrend4Hours + ' last4HoursClose: ' + last4HoursClose + ' mid4HoursClose: ' + mid4HoursClose);
 }
 
