00837a4eecf39cf80f13ff629893cf6440839582
milesholt <contact@milesholt.co.uk>
2024-12-12 11:58:06 +0800

    Update strategy2Handler.js

--- core/handlers/strategy2Handler.js
+++ core/handlers/strategy2Handler.js
@@ -66,9 +66,9 @@ actions.iniRun = async function () {
   console.log("Certainty:", result.certainty);
 
   //return result;
-  if((result.signal == 'BUY' || result.signal == 'SELL') && result.certainty >= 0.7){
+  if((result.signal == 'STRONG BUY' || result.signal == 'STRONG SELL') && result.certainty >= 0.7){
     console.log('Making trade...');
-    set.decision = result.signal;
+    set.decision = result.signal.replace('STRONG ', '');
     await actions.beginTrade(set);  
   } else {
     console.log('Did not make trade');
