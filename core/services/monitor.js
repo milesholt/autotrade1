3a3beda456d2759dc5cc3f7511989b48569df9de
milesholt <contact@milesholt.co.uk>
2025-02-05 03:25:54 +0800

    Update monitor.js

--- core/services/monitor.js
+++ core/services/monitor.js
@@ -98,7 +98,7 @@ actions.beginMonitor = async function(dealId,dealRef,epic,mid,streamLogDir,attem
 
   isStreamRunning[epic] = false;
 
-  markets[mid].trailingStop == false
+  markets[mid].trailingStop == false;
 
   console.log('epic: ' + arr.epic + ' mid: ' + arr.marketId +  ' streamingPricesAvailable:' + markets[arr.marketId].streamingPricesAvailable);
 
