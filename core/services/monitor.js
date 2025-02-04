f1f407088cb3f2610b3c489380209f5923b290d7
milesholt <contact@milesholt.co.uk>
2025-02-05 02:28:34 +0800

    Update monitor.js

--- core/services/monitor.js
+++ core/services/monitor.js
@@ -480,7 +480,7 @@ actions.beginMonitor = async function(dealId,dealRef,epic,mid,streamLogDir,attem
                                    let trailingStopIncrement = trailingStopDistance * 0.1; // Example: Increment is 10% of distance
 
                                    let updateData = {
-                                        guaranteedStop: false,  // Required constraint
+                                        //guaranteedStop: false,  // Required constraint
                                         stopLevel: p.stopLevel,  // Required when enabling trailing stop
                                         limitLevel: p.limitLevel,
                                         trailingStop: true,
@@ -488,13 +488,14 @@ actions.beginMonitor = async function(dealId,dealRef,epic,mid,streamLogDir,attem
                                         trailingStopIncrement: trailingStopIncrement
                                     };
 /*
-                                   {
+{
     "stopLevel": "2788.85",
-    "limitLevel": null,
+    "limitLevel": "2873.38",
     "trailingStop": "true",
     "trailingStopDistance": "2789",
     "trailingStopIncrement": "278.88"
-}*/
+}
+*/
 
                             
                                     //Update position and switch to trailing stop
