85c6db0255a6e6f800c5c7a4fcd0451a71b7a308
milesholt <contact@milesholt.co.uk>
2025-02-25 02:04:34 +0900

    Update monitor.js

--- core/services/monitor.js
+++ core/services/monitor.js
@@ -520,6 +520,8 @@ let isTrailingStopDefined = lib.actions.isDefined(markets[x.marketId], 'trailing
 //Only proceed if trailingstop already tried
 if (isTrailingStopDefined) {
 
+            console.log('checking guaranteed thresholds');
+
             //Do adjustment checks for thresholds
 
             //Set default
