ddfa9ff98ab6f0a065442e84c2706681ab0d373d
milesholt <contact@milesholt.co.uk>
2025-03-22 00:12:41 +0900

    Update checkHandler.js

--- core/handlers/strategies/breakOut/checkHandler.js
+++ core/handlers/strategies/breakOut/checkHandler.js
@@ -72,6 +72,40 @@ actions.checkLastClosedTrade = async function(){
    });
 }
 
+/* GET CLOSED TRADES FROM MARKET
+
+
+actions.checkLastClosedTrades = async function(){
+   let pageSize = 20;
+   let type = 'ALL_DEAL';
+   let from = undefined;
+   let to = undefined;
+   await api.acctTransaction(type,from, to, pageSize,1).then(async r => {
+     let transactions = r.transactions;
+       
+     /*  
+     Example of closed transaction
+     {
+        date: '2025-03-14',
+        dateUtc: '2025-03-14T12:16:54',
+        openDateUtc: '2025-03-13T00:02:06',
+        instrumentName: 'MXN/JPY',
+        period: 'DFB',
+        profitAndLoss: '£41.89',
+        transactionType: 'DEAL',
+        reference: 'DIAAAAS285KGSBC',
+        openLevel: '735.4',
+        closeLevel: '743.1',
+        size: '+5.44',
+        currency: '£',
+        cashTransaction: false
+      }*/
+
+     return transactions;                      
+   });
+}
+
+
 /*
 
 CHECK OPEN TRADES
