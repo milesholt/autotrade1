ef824632c12eac6135132bc84f7885810ddd5910
milesholt <contact@milesholt.co.uk>
2025-03-22 00:21:11 +0900

    Update checkHandler.js

--- core/handlers/strategies/breakOut/checkHandler.js
+++ core/handlers/strategies/breakOut/checkHandler.js
@@ -48,12 +48,7 @@ actions.checkLastClosedTrade = async function(){
        let transactions = r.transactions;
 
      //get last transanction
-     //let lastTrade = transactions[0];
-
-     let lastTrade = transactions.filter((m) => m.instrumentName == 'MXN/JPY')[0];
-     console.log(markets[0].instrumentName);
-     console.log(lastTrade);
-     
+     let lastTrade = transactions[0];
     
      /*  
      Example of closed transaction
