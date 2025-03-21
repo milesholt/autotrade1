49a258cde058c891d60a462d106d8fa2abf405c0
milesholt <contact@milesholt.co.uk>
2025-03-22 04:36:51 +0900

    Update strategy2Handler2.js

--- core/handlers/strategy2Handler2.js
+++ core/handlers/strategy2Handler2.js
@@ -105,7 +105,7 @@ actions.iniRun = async function () {
 
   //Filter last by instrumentName
   let lastTrade = null;
-  lastTrade = lastTrades.filter((m) => m.instrumentName == market.instrumentName)[0]; 
+  if(lastTrades.length) lastTrade = lastTrades.filter((m) => m.instrumentName == market.instrumentName)[0]; 
   
   if(lastTrade !== null){
     console.log('Found last closed trade');
