220266bf017fddcd73726f31e85eb436f2c3bcc5
milesholt <contact@milesholt.co.uk>
2025-04-04 00:43:40 +0900

    Update checkHandler.js

--- core/handlers/strategies/breakOut/checkHandler.js
+++ core/handlers/strategies/breakOut/checkHandler.js
@@ -89,6 +89,11 @@ actions.checkLastClosedTrades = async function(){
 
     let from = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000).toISOString(); // 14 days ago
     let to = new Date().toISOString(); // Current time
+
+  console.log('from');
+  console.log(from);
+  console.log('to:');
+  console.log(to);
   
    return await api.acctTransaction(type,from, to, pageSize,1).then(r => {
        let transactions = r.transactions;
