a964c0ce4b8172d5c8f39808de2aedabdf6fa929
milesholt <contact@milesholt.co.uk>
2025-04-11 01:21:30 +0900

    Update autotrade2.js

--- core/autotrade2.js
+++ core/autotrade2.js
@@ -348,7 +348,7 @@ actions.init = async function () {
 
   await api.acctInfo()
         .then((r)=> {
-          accountBalance = r.accounts[0].balance;
+          accountBalance = r.accounts[0].balance.balance;
           console.log('account balance: ' + accountBalance);
         });
   
