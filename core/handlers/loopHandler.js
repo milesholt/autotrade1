b3954ee69be0bb33f07c10332672b6899c13cdb8
milesholt <contact@milesholt.co.uk>
2025-04-15 02:34:19 +0900

    Update loopHandler.js

--- core/handlers/loopHandler.js
+++ core/handlers/loopHandler.js
@@ -29,14 +29,6 @@ actions.loop = async function(msg = ''){
   let timestamp  = moment.utc().local().format('LLL');
   console.log('Time is:' + timestamp);
 
-  await api.acctInfo()
-        .then((r)=> {
-          accountBalance = r.accounts[0].balance.available;
-          console.log('account balance: ' + accountBalance);
-        });
-  
-
-
   //check loop isn't already running
   // if(!!isLoopRunning){
   //   isLoopRunning = true;
