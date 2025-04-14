7817bc8172c1ce72c764faaf4dc1ecde4025dc3e
milesholt <contact@milesholt.co.uk>
2025-04-15 01:01:24 +0900

    Update ig.js

--- core/services/ig.js
+++ core/services/ig.js
@@ -12,6 +12,35 @@ actions.login = function(){
   });
 }
 
+actions.loginLive = function(){
+
+  console.log('Logging in live');
+
+  process.env.IG_IDENTIFIER = process.env.IG_IDENTIFIER_LIVE;
+  process.env.IG_PASSWORD = process.env.IG_PASSWORD_LIVE;
+  process.env.IG_API_KEY = process.env.IG_API_KEY_LIVE;
+  process.env.IG_DEMO = "FALSE";
+  
+  return new Promise((resolve, reject) => {
+    api.login(true).then(r => resolve(r)).catch(e => reject(e));
+  });
+}
+
+actions.loginDemo = function(){
+
+  console.log('Logging in demo');
+
+  process.env.IG_IDENTIFIER = process.env.IG_IDENTIFIER_DEMO;
+  process.env.IG_PASSWORD = process.env.IG_PASSWORD_DEMO;
+  process.env.IG_API_KEY = process.env.IG_API_KEY_DEMO;
+  process.env.IG_DEMO = "TRUE";
+  
+  return new Promise((resolve, reject) => {
+    api.login(true).then(r => resolve(r)).catch(e => reject(e));
+  });
+}
+
+
 module.exports = {
   actions: actions,
   api: api
