80590427645a0268c1892d0228588a8939cd476e
milesholt <contact@milesholt.co.uk>
2025-04-16 13:38:55 +0000

    Update liveHandler.js

--- core/handlers/liveHandler.js
+++ core/handlers/liveHandler.js
@@ -109,7 +109,7 @@ actions.openLivePosition = async function (ticket) {
       if(response.confirms.reason == 'MINIMUM_ORDER_SIZE_ERROR'){
           //Minumum sizes can be different on live account, so update minimum size and try again
           await api.epicDetails([epic]).then(async (r) => {
-                let minimumSize = r.marketDetails[0].dealingRules.minDealSize;
+                let minimumSize = r.marketDetails[0].dealingRules.minDealSize.value;
 
                 console.log('live minimum size', minimumSize);
             
