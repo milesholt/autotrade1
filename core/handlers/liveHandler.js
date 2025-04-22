e26e6d3606b90fb9ee535f20d4e2525ca0160988
milesholt <contact@milesholt.co.uk>
2025-04-22 14:41:58 +0900

    Update liveHandler.js

--- core/handlers/liveHandler.js
+++ core/handlers/liveHandler.js
@@ -84,7 +84,7 @@ actions.preLive = async function(){
     //Get market details
     await api.epicDetails([m.epic]).then(async (r) => {
             
-                let minimumSize = r.marketDetails[0].dealingRules.minDealSize.value;
+                let minimumSize = r.marketDetails[0].dealingRules.minDealSize;
                 let stopDistance = r.marketDetails[0].dealingRules.minNormalStopOrLimitDistance;
 
                 console.log('epic', m.epic);
