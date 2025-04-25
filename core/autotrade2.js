5dbc9e672815aa1f49bd9c6c47726f7b742e8461
milesholt <contact@milesholt.co.uk>
2025-04-26 00:18:08 +0900

    Update autotrade2.js

--- core/autotrade2.js
+++ core/autotrade2.js
@@ -353,8 +353,6 @@ actions.init = async function () {
   await api.getPosition(String(dealId)).then(async positionData => {
       console.log(util.inspect(positionData, false, null));
   }).catch(async e => {
-
-      console.log('Error finding position with dealId: ' + dealId);
       console.log(e.body.errorCode);
   });
 
