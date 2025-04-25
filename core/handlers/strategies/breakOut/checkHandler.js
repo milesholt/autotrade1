dd3442a2046ba3a88fe7dfdb1f01f79cda751998
milesholt <contact@milesholt.co.uk>
2025-04-26 00:38:50 +0900

    Update checkHandler.js

--- core/handlers/strategies/breakOut/checkHandler.js
+++ core/handlers/strategies/breakOut/checkHandler.js
@@ -633,6 +633,9 @@ actions.checkOpenTrade = async function(mrk,ep,tmid){
 
                   if(markets[mon.marketId].streamingPricesAvailable == true){
                     console.log('Open trade wasnt monitoring, starting monitoring. dealRef: ' + dealRef + ' dealId: ' + dealId + ' epic: ' + mon.epic);
+                    //log orginal stopLevel if doesn't exist.
+                    if(!lib.isDefined(markets[mon.marketId].ticket, 'stopLevel')) markets[mon.marketId].ticket.stopLevel = positionData.position.stopLevel;
+                    
                     await monitor.iniMonitor(dealId, dealRef, mon.epic, mon.marketId);
                   }
 
