26ee35e47b60532a64dacfb0dccd471c44fb2964
milesholt <contact@milesholt.co.uk>
2025-04-27 00:24:22 +0900

    Update monitor.js

--- core/services/monitor.js
+++ core/services/monitor.js
@@ -19,6 +19,7 @@ const mailer = require('./mailer.js');
 const lib = require('./library.js');
 const log = require('./log.js');
 const check = require('../handlers/strategies/breakOut/checkHandler.js');
+const strategy2 = require('../handlers/strategy2Handler2.js');
 const github = require('./github.js');
 const testmailer = require('../tests/mailer.js');
 //Stream log
@@ -802,6 +803,61 @@ if (isTrailingStopDefined) {
 
                                 //console.log('stream data: ');
                                 //console.log(d);
+
+                                //Get Strategy2 data from market
+                                const m = markets[x.marketId].data.strategy2;                           
+                                  
+                                if(m.makeTrade === true){              
+                                                              
+                                  try {
+                                        const tradeParams = {
+                                          entryPrice: closePrice,
+                                          desiredLossAmount: desiredLossAmount,      
+                                          desiredProfitAmount: desiredProfitAmount,    
+                                          accountEquity: accountBalance,    
+                                          marketInfo: markets[x.marketId],        
+                                          direction: dir,    
+                                        };
+                                    
+                                        const tradeDetails = await strategy2.actions.calculateTradeDetails(tradeParams, set);
+
+                                        if (dir === "BUY") {
+                                          m.limitLevel = closePrice + tradeDetails.limitDistance;
+                                          m.stopLevel = closePrice - tradeDetails.stopDistance;
+                                        } else if (dir === "SELL") {
+                                          m.limitLevel = closePrice - tradeDetails.limitDistance;
+                                          m.stopLevel = closePrice + tradeDetails.stopDistance;
+                                        }
+                                        
+                                        //If strategy2 would make trade, continue rather than closing
+                                        let adjustPositionData = {
+                                            "stopLevel": m.stopLevel,
+                                            "limitLevel": m.limitLevel,
+                                        }
+        
+                                        await api.editPosition(x.dealId, adjustPositionData).then(r => {
+                                            if (r.dealStatus == 'ACCEPTED') {
+                                                console.log("Adjusted position after reaching profit");  
+                                              
+                                                //Restart monitor once position updated, new position details should be fetched by api
+                                                stream.actions.unsubscribe(monitorData.epic);
+                                                monitorData.subscribed = false;
+                                                isStreamRunning[monitorData.epic] = false;
+                                                actions.beginMonitor(monitorData.dealId,monitorData.dealRef,monitorData.epic,monitorData.marketId,monitorData.streamLogDir,true);
+                                               
+                                            } else {
+                                                console.log("Failed to apply adjusted position after reaching profit");
+                                                //continue to close is failed to adjust position
+                                            }
+                                        }).catch(e => console.log(e));
+                                        
+                                  } catch (error) {
+                                        console.error("Error, unable to calulateTradeDetails for adjust position:", error.message);
+                                        //continue to close position if unable to adjust
+                                  }
+                                                     
+                                }  
+                                  
 
                                 //NOTE - Make sure to clear monitordata if you update limitClosePerc or stopClosePerc on config, as tmp monitor data is not updated with new limits or stops unless monitor restarts.
 
