4a296558fc2f9d308aa393e14521f28e2d767641
milesholt <contact@milesholt.co.uk>
2025-06-09 18:14:30 +0900

    Update analytics.js

--- core/services/analytics.js
+++ core/services/analytics.js
@@ -37,7 +37,7 @@ DRAW CHART
 
 */
 
-actions.drawChart = async function(priceData, lineData, analysis, rangeData){
+actions.drawChart = async function(priceData, lineData, analysisDataSet, rangeData){
 
   //priceData = priceData.filter(price => price.open !== 0 && price.close !== 0 && price.high !== 0 && price.low !== 0);
 
