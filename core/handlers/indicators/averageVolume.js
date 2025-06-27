95e66f953e9e6026f86eb908c3e7d6ba949eca5d
milesholt <contact@milesholt.co.uk>
2025-06-27 18:11:39 +0900

    Update averageVolume.js

--- core/handlers/indicators/averageVolume.js
+++ core/handlers/indicators/averageVolume.js
@@ -1,1 +1,15 @@
+var actions = {};
 
+actions.calculateAverageVolume = async function(data, period) {
+  if (data.length < period) {
+    throw new Error("Not enough data to calculate the average volume for the specified period.");
+  }
+
+  const recentData = data.slice(data.length - period);
+  const totalVolume = recentData.reduce((sum, item) => sum + item.volume, 0);
+  return totalVolume / period;
+};
+
+module.exports = {
+  actions:actions
+}
