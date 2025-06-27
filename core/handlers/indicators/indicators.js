431db5dcad86b1318ded835049d41967ac78f2d5
milesholt <contact@milesholt.co.uk>
2025-06-27 17:44:32 +0900

    Update indicators.js

--- core/handlers/indicators/indicators.js
+++ core/handlers/indicators/indicators.js
@@ -1,4 +1,19 @@
-var actions ={};
+var actions = {};
+
+//Tools
+
+var macd = require('./macd.js');
+var atr = require('./atr.js');
+var momentum = require('./momentum.js');
+var ema = require('./ema.js');
+var sma = require('./sma.js');
+var fibonacci = require('./fibonacci.js');
+var bollinger = require('./bollinger.js');
+var rsi = require('./rsi.js');
+
+actions.doTechnicalAnalysis(prices){
+  
+}
 
 module.exports = {
   actions: actions
