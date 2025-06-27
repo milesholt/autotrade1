5d5eb307c69cad102126b492851808cfa3b19853
milesholt <contact@milesholt.co.uk>
2025-06-27 18:09:40 +0900

    Update roc.js

--- core/handlers/indicators/roc.js
+++ core/handlers/indicators/roc.js
@@ -1,1 +1,13 @@
+var actions = {};
+
+actions.calculateROC = async function(prices, period) {
+  if (prices.length < period) return null; // Not enough data
+  const currentPrice = prices[prices.length - 1];
+  const pastPrice = prices[prices.length - 1 - period];
+  return ((currentPrice - pastPrice) / pastPrice) * 100;
+}
+
+module.exports = {
+  actions : actions,
+}
 
