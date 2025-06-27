eefced2cf2a5592290c037ac2ffe9267bae2a527
milesholt <contact@milesholt.co.uk>
2025-06-27 17:17:58 +0900

    Update bollinger.js

--- core/handlers/indicators/bollinger.js
+++ core/handlers/indicators/bollinger.js
@@ -1,1 +1,19 @@
+var actions = {};
+
+actions.calculateBollingerBands = async function(data, period, multiplier) {
+    const sma = await actions.calculateSMA(data, period);
+    const stdDev = [];
+
+    for (let i = period - 1; i < data.length; i++) {
+      const slice = data.slice(i - period + 1, i + 1);
+      const mean = sma[i - period + 1];
+      const variance = slice.reduce((sum, item) => sum + Math.pow(item.close - mean, 2), 0) / period;
+      stdDev.push(Math.sqrt(variance));
+    }
+
+    const upper = sma.map((val, index) => val + multiplier * stdDev[index]);
+    const lower = sma.map((val, index) => val - multiplier * stdDev[index]);
+    return { upper, lower };
+  };
+
 
