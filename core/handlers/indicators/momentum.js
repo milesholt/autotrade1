df4c7e92f746f61cb0febfe7286963847e3f3e18
milesholt <contact@milesholt.co.uk>
2025-06-27 17:38:16 +0900

    Update momentum.js

--- core/handlers/indicators/momentum.js
+++ core/handlers/indicators/momentum.js
@@ -1,1 +1,10 @@
+var actions = {};
+
+actions.calculateMomentum = async function(prices, period) {
+  const smoothedCurrent = await actions.calculateSMARecent(prices.slice(-period), period);
+  const smoothedPast = await actions.calculateSMARecent(prices.slice(0, -period), period);
+  if (smoothedCurrent === null || smoothedPast === null) return null; // Not enough data
+  return smoothedCurrent - smoothedPast;
+}
+
 
